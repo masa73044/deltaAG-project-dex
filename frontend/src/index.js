e91c1dc630ce6578394b7c003c640977155c74c5
masa73044 <masa7304@gmail.com>
2023-04-06 10:36:49 +0700

    header completed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,27 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { createClient, configureChains, mainnet, WagmiConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+
+const { provider, webStocketProvider } = configureChains(
+  [mainnet],
+  [publicProvider()]
+);
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+  webStocketProvider,
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <WagmiConfig client={client}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+    </WagmiConfig>
   </React.StrictMode>
 );
