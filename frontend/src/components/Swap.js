0f38177acb88390c3c7b3af0466f768454c70bf8
masa73044 <masa7304@gmail.com>
2023-04-10 19:39:39 +0700

    options added

--- frontend/src/components/Swap.js
+++ frontend/src/components/Swap.js
@@ -3,8 +3,6 @@ import axios from "axios";
 import { useSendTransaction, useWaitForTransaction } from "wagmi";
 import tokenList from "../tokenList.json";
 import WindowSwap from "./WindowSwap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCog } from "@fortawesome/free-solid-svg-icons";
 
 function Swap(props) {
   const { address, isConnected } = props;
@@ -38,104 +36,119 @@ function Swap(props) {
     hash: data?.hash,
   });
 
-  // new
-  const WindowWithGearButton = () => {
-    const [dropdownVisible, setDropdownVisible] = useState(false);
+  function handleSlippageChange(e) {
+    setMenuOpen(false); // new feature
 
-    const toggleDropdown = () => {
-      setDropdownVisible(!dropdownVisible);
-    };
+    setSlippage(e.target.value);
+  }
 
-    function handleSlippageChange(e) {
-      setMenuOpen(false); // new feature
-
-      setSlippage(e.target.value);
-    }
-
-    // new
-
-    function changeAmount(e) {
-      setTokenOneAmount(e.target.value);
-      if (e.target.value && prices) {
-        setTokenTwoAmount((e.target.value * prices.ratio).toFixed(2));
-      } else {
-        setTokenTwoAmount(null);
-      }
-    }
-
-    function switchTokens() {
-      setPrices(null);
-      setTokenOneAmount(null);
+  function changeAmount(e) {
+    setTokenOneAmount(e.target.value);
+    if (e.target.value && prices) {
+      setTokenTwoAmount((e.target.value * prices.ratio).toFixed(2));
+    } else {
       setTokenTwoAmount(null);
-      const one = tokenOne;
-      const two = tokenTwo;
-      setTokenOne(two);
-      setTokenTwo(one);
-      fetchPrices(two.address, one.address);
     }
+  }
 
-    function openModal(asset) {
-      setChangeToken(asset);
-      setIsOpen(true);
+  function switchTokens() {
+    setPrices(null);
+    setTokenOneAmount(null);
+    setTokenTwoAmount(null);
+    const one = tokenOne;
+    const two = tokenTwo;
+    setTokenOne(two);
+    setTokenTwo(one);
+    fetchPrices(two.address, one.address);
+  }
+
+  function openModal(asset) {
+    setChangeToken(asset);
+    setIsOpen(true);
+  }
+
+  function modifyToken(i) {
+    setPrices(null);
+    setTokenOneAmount(null);
+    setTokenTwoAmount(null);
+    if (changeToken === 1) {
+      setTokenOne(tokenList[i]);
+      fetchPrices(tokenList[i].address, tokenTwo.address);
+    } else {
+      setTokenTwo(tokenList[i]);
+      fetchPrices(tokenOne.address, tokenList[i].address);
     }
+    setIsOpen(false);
+  }
 
-    function modifyToken(i) {
-      setPrices(null);
-      setTokenOneAmount(null);
-      setTokenTwoAmount(null);
-      if (changeToken === 1) {
-        setTokenOne(tokenList[i]);
-        fetchPrices(tokenList[i].address, tokenTwo.address);
-      } else {
-        setTokenTwo(tokenList[i]);
-        fetchPrices(tokenOne.address, tokenList[i].address);
-      }
-      setIsOpen(false);
-    }
+  // new functions
 
-    async function fetchPrices(one, two) {
-      const res = await axios.get(`http://localhost:3001/tokenPrice`, {
-        params: { addressOne: one, addressTwo: two },
-      });
-      setPrices(res.data);
-    }
+  async function fetchPrices(one, two) {
+    const res = await axios.get(`http://localhost:3001/tokenPrice`, {
+      params: { addressOne: one, addressTwo: two },
+    });
+    setPrices(res.data);
+  }
 
-    // new functions
+  const settings = (
+    <>
+      <button className="dropdown-btn" onClick={() => setMenuOpen(!menuOpen)}>
+        Slippage Tolerance
+      </button>
+      {menuOpen && (
+        <div className="slippage-options" onChange={handleSlippageChange}>
+          <div>
+            <button value={0.5}> 0.5%</button>
+            <label htmlFor="slippage-0.5">0.5%</label>
+          </div>
+          <div>
+            <input
+              name="slippage"
+              value={2.5}
+              checked={slippage === 2.5}
+              onChange={handleSlippageChange}
+            />
+            <label htmlFor="slippage-2.5">2.5%</label>
+          </div>
+          <div>
+            <input
+              name="slippage"
+              value={5}
+              checked={slippage === 5}
+              onChange={handleSlippageChange}
+            />
+            <label htmlFor="slippage-5">5%</label>
+          </div>
+        </div>
+      )}
+    </>
+  );
 
-    const WindowWithGearButton = () => {
-      const [dropdownVisible, setDropdownVisible] = useState(false);
-
-      const toggleDropdown = () => {
-        setDropdownVisible(!dropdownVisible);
-      };
-    };
-    return (
-      <>
-        <div>
-          <button onClick={() => setIsOpen(true)}>Open Modal</button>
-          <WindowSwap
-            open={isOpen}
-            title="Select a Token"
-            onCancel={() => setIsOpen(false)}
-          ></WindowSwap>
-          <div class="window">
-            <div class="title-bar">Delta Swap</div>
-            <div class="content"></div>
-            <div className="gear-btn" onClick={toggleDropdown}>
-              <FontAwesomeIcon icon={faCog} />
-            </div>
-            {dropdownVisible && (
-              <div className="dropdown-menu">
+  return (
+    <>
+      <div>
+        {/* <button onClick={() => setIsOpen(true)}>Open Modal</button>
+        <WindowSwap
+          open={isOpen}
+          title="Select a Token"
+          onCancel={() => setIsOpen(false)}
+        ></WindowSwap> */}
+        <div class="window">
+          <div class="title-bar">Delta Swap</div>
+          <div class="content">
+            <div class="dropdown">
+              <button class="dropdown-button">Options</button>
+              <div class="dropdown-content">
                 <a href="#">Option 1</a>
                 <a href="#">Option 2</a>
                 <a href="#">Option 3</a>
               </div>
-            )}
+            </div>
           </div>
         </div>
-      </>
-    );
-  };
+      </div>
+    </>
+  );
 }
 
 export default Swap;
