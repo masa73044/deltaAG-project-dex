18718cfeb8c63ddf60191e09ed21aa0e07251ed1
masa73044 <masa7304@gmail.com>
2023-04-11 17:29:54 +0700

    index.js finished

--- backend/index.js
+++ backend/index.js
@@ -9,11 +9,30 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/tokenPrice", async (req, res) => {
-  return res.status(200).json(usdPrices);
+  try {
+    const { query } = req;
+
+    const responseOne = await Moralis.EvmApi.token.getTokenPrice({
+      address: query.addressOne,
+    });
+
+    const responseTwo = await Moralis.EvmApi.token.getTokenPrice({
+      address: query.addressTwo,
+    });
+
+    const usdPrices = {
+      tokenOne: responseOne.raw.usdPrice,
+      tokenTwo: responseTwo.raw.usdPrice,
+      ratio: responseOne.raw.usdPrice / responseTwo.raw.usdPrice,
+    };
+    return res.status(200).json(usdPrices);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 Moralis.start({
-  apiKey: process.env.DELTA_AG_KEY,
+  apiKey: process.env.MORALIS_KEY,
 }).then(() => {
   app.listen(port, () => {
     console.log(`Listening for API Calls`);
