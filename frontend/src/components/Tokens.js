c7e1b3fd67035c62fbc2bb87022b344cd2799fc7
masa73044 <108378047+masa73044@users.noreply.github.com>
2023-06-14 14:52:38 +0700

    updated UI for swap page

--- frontend/src/components/Tokens.js
+++ frontend/src/components/Tokens.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import tokenList from "../tokenList.json";
 
 function Tokens() {
   return (
-    <div>Tokens</div>
-  )
+    <>
+      <ul>
+        <div>Tokens</div>
+
+        {tokenList?.map((e, i) => {
+          return (
+            <li className="tokenChoice" key={i}>
+              <img src={e.img} alt={e.ticker} className="tokenLogo" />
+              <div className="tokenChoiceNames">
+                <div className="tokenName">{e.name}</div>
+                <div className="tokenTicker">{e.ticker}</div>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
 
-export default Tokens
+export default Tokens;
